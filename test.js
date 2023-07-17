2a32060b4ac2aa6a803e43280f80a371e6df29df
Pastakun <130649332+Pastakun@users.noreply.github.com>
2023-07-17 13:32:52 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,27 @@
+const id = Math.random().toString(32).substring(2);
+const ws = new WebSocket("wss://cloud.achex.ca/Pascha");
+ws.addEventListener('open',function(e) {
+    console.log('open');
+    ws.send(JSON.stringify({'auth': 'Pascha', 'password': 'pass'}));
+});
+ws.addEventListener('message',function(e) {
+    const obj = JSON.parse(e.data);
+    if(obj.auth == 'OK'){
+        return;
+    }
+    document.getElementsByClassName('chatscroll')[0].innerHTML += `<div><p>${obj.message}</p></div>`;
+    
+});
+function send(){
+    const a = document.getElementsByClassName('text')[0];
+    ws.send(JSON.stringify({'to': 'Pascha', 'message': a.value}));
+    a.value = '';
+}
+document.getElementsByClassName('send')[0].addEventListener('click', function(){
+    send();
+});
+document.getElementsByClassName('text')[0].addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    send();
+  }
+});
